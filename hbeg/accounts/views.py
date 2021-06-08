f2a30aa02305cbdccbf6ee3817cd96767222c39c
Yash Prakash <yashprakash13@gmail.com>
2021-06-08 21:49:11 +0530

    login/reg messages fixed.

--- hbeg/accounts/views.py
+++ hbeg/accounts/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import RegisterForm
 
 class RegisterView(View):
+    """View to register a user
+    """
     def get(self, request):
         return render(request, 'accounts/register.html', {'form': RegisterForm()})
 
@@ -18,16 +20,18 @@ class RegisterView(View):
             nickname, password = form.cleaned_data.get('nickname'), form.cleaned_data.get('password1')
             member = authenticate(nickname=nickname, password=password)
             login(self.request, member)
+            # go to profile page upon redirect
             return redirect('profile')
         else:
+            # check for registration errors
             password1 = form.data['password1']
             password2 = form.data['password2']
             nickname = form.data['nickname']
             for msg in form.errors.as_data():
                 if msg == 'nickname':
-                    messages.error(request, f"Chosen {nickname} is not valid")
+                    messages.error(request, f"Chosen nickname cannot be used as a valid nickname.")
                 if msg == 'password2' and password1 == password2:
-                    messages.error(request, f"Selected password: {password1} is not strong enough.")
+                    messages.error(request, f"Selected password: is not strong enough.")
                 elif msg == 'password2' and password1 != password2:
-                    messages.error(request, f"Password: '{password1}' and Confirmation Password: '{password2}' do not match.")
+                    messages.error(request, f"The Passwords do not match.")
             return render(request, 'accounts/register.html', {'form': RegisterForm()})
