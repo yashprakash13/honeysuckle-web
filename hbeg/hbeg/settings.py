662f73d257b29344c2a2308e85e8f3ca3f2029c7
Yash Prakash <yashprakash13@gmail.com>
2021-06-15 14:11:44 +0530

    new postgres db connection.

--- hbeg/hbeg/settings.py
+++ hbeg/hbeg/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'hbeg.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hbegproject',
+        'USER': 'hbegprojectuser',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
