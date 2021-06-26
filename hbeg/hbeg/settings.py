5d361f50e4d19e0f554defd391f61e73414e83c7
Yash Prakash <yashprakash13@gmail.com>
2021-06-26 19:53:08 +0530

    changed allowed hosts in settings.py

--- hbeg/hbeg/settings.py
+++ hbeg/hbeg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '134.209.156.80']
+ALLOWED_HOSTS = ['localhost', '134.209.156.80', 'hisbrowneyedgirl.com', 'www.hisbrowneyedgirl.com']
 
 # LOGGING = {
 #     'version': 1,
