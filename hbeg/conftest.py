383b13a12fa57d9e30f5555389fe812bff8ef063
Yash Prakash <yashprakash13@gmail.com>
2021-07-13 00:00:04 +0530

    added login/profile access test with selenium integration.

--- hbeg/conftest.py
+++ hbeg/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_factoryboy import register
+from selenium import webdriver
 
 from accounts.tests.factories import UserFactory
 from profiles.tests.factories import FolderFactory, StoryFactory
@@ -49,3 +50,32 @@ def get_new_folders(db, folder_factory, user_factory):
     folder2 = folder_factory.create(folder_name="test_folder_name2", folder_desc="test_folder_desc2", created_by=user)
     folder3 = folder_factory.create(folder_name="test_folder_name3", folder_desc="test_folder_desc3", created_by=user)
     return (folder1, folder2, folder3)
+
+
+# ============================LIVE TESTS==================================
+@pytest.fixture(autouse=False)
+def use_debug_true_for_selenium_tests(settings):
+    settings.DEBUG = True
+
+
+@pytest.fixture(params=["chrome1920"], scope="class")  # , "chrome411"
+def driver_init(request):
+    binaryPath = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+    if request.param == "chrome1920":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1080")
+        options.binary_location = binaryPath
+        web_driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
+        request.cls.browser = "Chrome1920x1080"
+    # if request.param == "chrome411":
+    #     options = webdriver.ChromeOptions()
+    #     options.add_argument("--headless")
+    #     options.add_argument("--window-size=411,823")
+    #     options.binary_location = binaryPath
+    #     web_driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
+    #     request.cls.browser = "Chrome411x823"
+
+    request.cls.driver = web_driver
+    yield
+    web_driver.close()
