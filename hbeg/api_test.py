e529472223c7f1df23a6c62f8dbedd9eb3548490
Yash Prakash <yashprakash13@gmail.com>
2021-07-19 11:35:54 +0530

    api_test modified.

--- hbeg/api_test.py
+++ hbeg/api_test.py
@@ -21,21 +21,41 @@ from bs4 import BeautifulSoup
 # get_story_details("https://www.fanfiction.net/s/2963991/1/Harry-Potter-and-the-Oroborus-Light")
 
 
-def get_new_stories():
-    url = "https://archiveofourown.org/tags/Hermione%20Granger*s*Harry%20Potter/works?page=1"
-
-    html = requests.get(url).content
-
-    soup = BeautifulSoup(html, "html.parser")
-
-    all_stories_ol = soup.find("ol", {"class": "work index group"}).findAll("li", recursive=False)
-    # print(all_stories_ol)
-    a = time.time()
-    for li in all_stories_ol:
-        workid = li.get("id")[5:]
-        work = AO3.Work(int(workid))
-        print(work.title)
-    print("Time taken: ", time.time() - a)
+# def get_story_details_ao3(story_id):
+#     response = requests.get(f"https://hisbrowneyedgirl.com/hsapi/ao3/{story_id}")
+#     data = json.loads(response.text)
+#     print(data)
 
 
-get_new_stories()
+# get_story_details_ao3("29403498")
+
+
+# def get_new_stories():
+#     url = "https://archiveofourown.org/tags/Hermione%20Granger*s*Harry%20Potter/works?page=1"
+
+#     html = requests.get(url).content
+
+#     soup = BeautifulSoup(html, "html.parser")
+
+#     all_stories_ol = soup.find("ol", {"class": "work index group"}).findAll("li", recursive=False)
+#     # print(all_stories_ol)
+#     a = time.time()
+#     for li in all_stories_ol:
+#         workid = li.get("id")[5:]
+#         work = AO3.Work(int(workid))
+#         print(work.title)
+#     print("Time taken: ", time.time() - a)
+
+
+# get_new_stories()
+
+
+def search_ao3(query):
+    search = AO3.Search(title=query, fandoms="Harry Potter", rating=13)
+    search.update()
+    print(search.total_results)
+    for result in search.results:
+        print(result)
+
+
+search_ao3("bonds of time")
