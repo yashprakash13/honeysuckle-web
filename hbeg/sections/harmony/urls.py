6a4b801161e6389144394a7a2427f68e2dbc684d
Yash Prakash <yashprakash13@gmail.com>
2021-07-22 20:37:50 +0530

    moments page made. Trial intended for prod.

--- hbeg/sections/harmony/urls.py
+++ hbeg/sections/harmony/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("harmony/", CentralPageView.as_view(), name="harmony_central_page"),
+    path("harmony/moments", MomentsView.as_view(), name="harmony_moments"),
 ]
--- hbeg/sections/harmony/views.py
+++ hbeg/sections/harmony/views.py
@@ -3,7 +3,9 @@ from threading import Thread
 from django.shortcuts import render
 from django.views import View
 
+from .dailyfeed.constants import HP_BOOK_CHOICES
 from .dailyfeed.feedmaker import FeedMaker
+from .models import *
 
 
 class CentralPageView(View):
@@ -12,3 +14,14 @@ class CentralPageView(View):
         feed = feed.get_feed()
         context = {"works": feed}
         return render(request, "harmony/main_view.html", context=context)
+
+
+class MomentsView(View):
+    def get(self, request):
+        books = [pair[1] for pair in HP_BOOK_CHOICES]
+        all_moments = {}
+        for book, choice in zip(books, HP_BOOK_CHOICES):
+            all_moments[book] = HarmonyMomentsModel.objects.filter(book__in=(choice)).values_list("moment", flat=True)
+        context = {"bookwise_moments": all_moments}
+
+        return render(request, "harmony/moments.html", context=context)
