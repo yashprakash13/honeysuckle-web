87ff2e001aeb1deefbd0ac370823f568790bb4bf
Yash Prakash <yashprakash13@gmail.com>
2021-07-28 20:30:16 +0530

    ao3 support for searcher added.

--- hbeg/core/searcher/settings.py
+++ hbeg/core/searcher/settings.py
@@ -2,7 +2,7 @@
 What data should the searcher have access to and use
 """
 
-DEBUGGING_WITHOUT_SEARCHER = True  # True if we were to debug without loading the full searcher module
+DEBUGGING_WITHOUT_SEARCHER = False  # True if we were to debug without loading the full searcher module
 
 OVERRIDE_FOR_HHR = True  # True if only for Harmony
 HHR_SEARCHER_PAIR = ["Harmony", "NoPairs"]  # None
